b82fe59e6152ae2c2e0a93d656fe845d4f6a5d8c
DeepakShettym <deepakshettyksd@gmail.com>
2025-04-22 12:45:53 +0530

    made changes in memory de-allocation

--- pcd_platform_driver.c
+++ pcd_platform_driver.c
@@ -9,6 +9,8 @@
 #include <linux/uaccess.h>
 #include <linux/slab.h>
 #include "platform.h"
+#include <linux/fs.h> 
+#include <linux/mod_devicetable.h>
 
 #undef pr_fmt
 #define pr_fmt(fmt) "%s : " fmt, __func__
@@ -26,6 +28,7 @@ struct pcdvr_private_data {
 	int total_devices;
 	dev_t device_num_base;
 	struct class *class_pcd;
+	struct device *device_pcd;
 };
 
 static struct pcdvr_private_data pcdrv_data;
@@ -71,6 +74,34 @@ static struct file_operations pcd_fops = {
 	.llseek = dms_llseek
 };
 
+
+struct device_config{
+        int config_item1;
+        int config_item2;
+};
+
+enum pcdev_names {
+        PCDEVA1X,
+        PCDEVB1X,
+        PCDEVC1X,
+        PCDEVD1X,
+};
+
+
+struct device_config pcdev_config[] ={
+        [PCDEVA1X]  = {.config_item1 = 60 , .config_item2 = 22},
+        [PCDEVB1X]  = {.config_item1 = 50 , .config_item2 = 23},
+        [PCDEVC1X]  = {.config_item1 = 40 , .config_item2 = 24},
+        [PCDEVD1X]  = {.config_item1 = 30 , .config_item2 = 25},
+};
+struct platform_device_id  pcdev_ids[] = {
+        {.name = "pcdev-A1x",.driver_data = PCDEVA1X},
+        {.name = "pcdev-B1x",.driver_data = PCDEVB1X},
+        {.name = "pcdev-C1x",.driver_data = PCDEVC1X},
+        {.name = "pcdev-D1x",.driver_data = PCDEVD1X}
+
+};
+
 // Platform Driver
 static int pcd_platform_driver_remove(struct platform_device *pdev){
 	struct pcdev_private_data *dev_data = dev_get_drvdata(&pdev->dev);
@@ -110,39 +141,51 @@ static int pcd_platform_driver_probe(struct platform_device *pdev) {
 	pr_info("device size = %d\n", dev_data->pdata.size);
 	pr_info("device permission = %d\n", dev_data->pdata.perm);
 
+	pr_info("config data = %d",pcdev_config[pdev->id_entry->driver_data].config_item1);
+	pr_info("config data = %d",pcdev_config[pdev->id_entry->driver_data].config_item2);
+
+
 	dev_data->buffer = devm_kzalloc(&pdev->dev,dev_data->pdata.size, GFP_KERNEL);
 	if (!dev_data->buffer) {
 		pr_info("cannot allocate memory for device buffer\n");
-		devm_kfree(&pdev->dev,dev_data);
-		return -ENOMEM;
+		return -ENOMEM; 
 	}
 
 	dev_data->dev_num = pcdrv_data.device_num_base + pdev->id;
+	
+	pr_info("Device ID: major=%d, minor=%d\n", MAJOR(pcdrv_data.device_num_base), MINOR(pcdrv_data.device_num_base));
 
 	cdev_init(&dev_data->cdev, &pcd_fops);
 	dev_data->cdev.owner = THIS_MODULE;
 	ret = cdev_add(&dev_data->cdev, dev_data->dev_num, 1);
 	if (ret < 0) {
 		pr_info("cdev add failed\n");
-		devm_kfree(&pdev->dev,dev_data->buffer);
-		devm_kfree(&pdev->dev,dev_data);
 		return ret;
 	}
 
-	device_create(pcdrv_data.class_pcd, NULL, dev_data->dev_num, NULL, "pcdev-%d", pdev->id);
-
+	pcdrv_data.device_pcd = device_create(pcdrv_data.class_pcd, NULL, dev_data->dev_num, NULL, "pcdev-%d", pdev->id);
+	if(IS_ERR(pcdrv_data.device_pcd)){
+		pr_err("Device create failed\n");
+		ret = PTR_ERR(pcdrv_data.device_pcd);
+		cdev_del(&dev_data->cdev);
+		return ret;
+	}
 	pcdrv_data.total_devices++;
 	pr_info("probe was successful\n");
 
 	return 0;
 }
 
+
 static struct platform_driver pcd_platform_driver = {
 	.probe = pcd_platform_driver_probe,
 	.remove = pcd_platform_driver_remove,
+	.id_table = pcdev_ids,  // bind using platfrom_match() 
+
 	.driver = {
 		.name = "pseudo-char-device"
 	}
+
 };
 
 // Module Initialization
@@ -153,6 +196,7 @@ static int __init hello_init(void) {
 	pr_info("Kernel Module Loaded\n");
 
 	result = alloc_chrdev_region(&pcdrv_data.device_num_base, 0, MAX_DEVICES, "pcdevs");
+	pr_info("Device ID: major=%d, minor=%d\n", MAJOR(pcdrv_data.device_num_base), MINOR(pcdrv_data.device_num_base));
 	if (result < 0) {
 		pr_err("Cannot allocate char dev\n");
 		return result;
